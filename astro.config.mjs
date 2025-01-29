57b98641a53c13ac3771b977b16654dbcb6d163f
Alexander Ulbrich <adulbrich@users.noreply.github.com>
2025-01-29 13:55:25 -0800

    added svelte and forms to compute assignment 2 problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,55 +4,53 @@ import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://engr103.alexulbrich.com",
-  integrations: [
-    sitemap(),
-    starlight({
-      title: "ENGR 103 Engineering Computation and Algorithmic Thinking",
-      social: {
-        github: "https://github.com/adulbrich/engr103",
+  integrations: [sitemap(), starlight({
+    title: "ENGR 103 Engineering Computation and Algorithmic Thinking",
+    social: {
+      github: "https://github.com/adulbrich/engr103",
+    },
+    head: [
+      {
+        tag: "script",
+        attrs: {
+          src: "/knowledge/js/script.outbound-links.js",
+          "data-api": "/knowledge/api/event",
+          "data-domain": "engr103.alexulbrich.com",
+          defer: true,
+        },
       },
-      head: [
-        {
-          tag: "script",
-          attrs: {
-            src: "/knowledge/js/script.outbound-links.js",
-            "data-api": "/knowledge/api/event",
-            "data-domain": "engr103.alexulbrich.com",
-            defer: true,
-          },
-        },
-      ],
-      sidebar: [
-        {
-          label: "Overview",
-          autogenerate: { directory: "overview" },
-        },
-        {
-          label: "Practicalities",
-          autogenerate: { directory: "practicalities" },
-        },
-        {
-          label: "Lecture Notes",
-          autogenerate: { directory: "lectures" },
-        },
-        {
-          label: "Studios",
-          autogenerate: { directory: "studios" },
-        },
-        {
-          label: "Assignments",
-          autogenerate: { directory: "assignments" },
-        },
-        {
-          label: "About",
-          autogenerate: { directory: "about" },
-        },
-      ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
-  ],
+    ],
+    sidebar: [
+      {
+        label: "Overview",
+        autogenerate: { directory: "overview" },
+      },
+      {
+        label: "Practicalities",
+        autogenerate: { directory: "practicalities" },
+      },
+      {
+        label: "Lecture Notes",
+        autogenerate: { directory: "lectures" },
+      },
+      {
+        label: "Studios",
+        autogenerate: { directory: "studios" },
+      },
+      {
+        label: "Assignments",
+        autogenerate: { directory: "assignments" },
+      },
+      {
+        label: "About",
+        autogenerate: { directory: "about" },
+      },
+    ],
+    customCss: ["./src/tailwind.css"],
+  }), tailwind({ applyBaseStyles: false }), svelte()],
 });
